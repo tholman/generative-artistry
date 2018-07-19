b18b1b2c0e0df027903927693573030da56427e3
Tim Holman <timothy.w.holman@gmail.com>
2018-07-19 00:20:55 -0400

    clean up generative favicon

--- themes/generative-artistry/static/js/favicon.js
+++ themes/generative-artistry/static/js/favicon.js
@@ -1,32 +1,34 @@
 (function () {
+  // Create canvas & context
   var canvas = document.createElement('canvas')
   var size = 20
   canvas.height = canvas.width = size
-
   var context = canvas.getContext('2d')
-  var img = document.createElement('img')
   
+  // Favicon element
   var link = document.createElement('link')
   link.rel = 'icon'
   link.type = 'image/png'
 
+  // Loop through and draw random diagonals
   var step = 4
   for( var x = 0; x < size; x += step) {
     for( var y = 0; y < size; y+= step ) {
-      var leftToRight = Math.random() >= 0.5;
+      var leftToRight = Math.random() >= 0.5
 
       if( leftToRight ) {
-        context.moveTo(x, y);
-        context.lineTo(x + step, y + step);    
+        context.moveTo(x, y)
+        context.lineTo(x + step, y + step)
       } else {
-        context.moveTo(x + step, y);
-        context.lineTo(x, y + step);
+        context.moveTo(x + step, y)
+        context.lineTo(x, y + step)
       }
 
-      context.stroke();
+      context.stroke()
     }
   }
 
-  link.href = canvas.toDataURL('image/png');
-  document.head.appendChild(link);
+  // Save to favicon and append to page
+  link.href = canvas.toDataURL('image/png')
+  document.head.appendChild(link)
 })();
